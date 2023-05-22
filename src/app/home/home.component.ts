464b4e6d20ab56504cb741e8948928c2252aee72
aetox <core.editor>
2023-05-22 11:26:03 +0200

    master

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, Inject, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HousingLocationComponent } from '../housing-location/housing-location.component';
-
+import { HousingLocation } from '../housinglocation';
+import { HousingService } from '../housing.service';
 @Component({
   selector: 'app-home',
   standalone: true,
@@ -14,11 +15,18 @@ import { HousingLocationComponent } from '../housing-location/housing-location.c
     </form>
   </section>
   <section class="results">
-    <app-housing-location></app-housing-location>
+    <app-housing-location *ngFor="let housingLocation of 
+    housingLocationList" [housingLocation]="housingLocation"></app-housing-location>
   </section>
   `,
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  housingLocationList: HousingLocation[] = [];
+  housingService: HousingService = inject(HousingService);
 
+  constructor(){
+    this.housingLocationList = this.housingService.getAllHousingLocations();
+  }
 }
+
